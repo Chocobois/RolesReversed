a50f865999551c8bc094a611b5811e86aa7eb7b0
Golen <golen87@hotmail.com>
2023-07-08 03:18:27 +0200

    Add wobble effect to hero and princess

--- src/rooms/HeroRoom.ts
+++ src/rooms/HeroRoom.ts
@@ -10,6 +10,7 @@ export class HeroRoom extends Phaser.GameObjects.Container {
 	public scene: GameScene;
 
 	public background: Phaser.GameObjects.Image;
+	public heroButton: Button;
 	public heroImage: Phaser.GameObjects.Image;
 
 	private heroState: HeroState;
@@ -24,8 +25,14 @@ export class HeroRoom extends Phaser.GameObjects.Container {
 		this.add(this.background);
 		scene.fitToScreen(this.background);
 
-		this.heroImage = scene.add.image(0.2 * scene.W, 0.75 * scene.H, 'hero_normal');
-		this.add(this.heroImage);
+		this.heroButton = new Button(scene, 0.2 * scene.W, 0.93 * scene.H);
+		this.add(this.heroButton);
+
+		this.heroImage = scene.add.image(0, 0, 'hero_default');
+		this.heroImage.setOrigin(0.5, 1.0);
+		this.heroButton.add(this.heroImage);
+		this.heroButton.bindInteractive(this.heroImage, false);
+		this.heroButton.on('click', this.onHeroClick, this);
 
 		this.timer = this.scene.time.addEvent({
 			delay: 0,
@@ -37,7 +44,12 @@ export class HeroRoom extends Phaser.GameObjects.Container {
 		this.setHeroState(HeroState.Present);
 	}
 
-	update(time: number, delta: number) {}
+	update(time: number, delta: number) {
+		const heroHoldX = 1.0 + 0.15 * this.heroButton.holdSmooth;
+		const heroHoldY = 1.0 - 0.1 * this.heroButton.holdSmooth;
+		const heroSquish = 0.02;
+		this.heroButton.setScale((1.0 + heroSquish * Math.sin(time / 200)) * heroHoldX, (1.0 + heroSquish * Math.sin(-time / 200)) * heroHoldY);
+	}
 
 	setVisible(isShown: boolean): this {
 		if (isShown) {
@@ -63,7 +75,7 @@ export class HeroRoom extends Phaser.GameObjects.Container {
 		});
 	}
 
-	// The player is not looking and the princess may perform an action
+	// The player is not looking and the hero may perform an action
 	movementOpportunity() {
 		function chance(odds: number) {
 			return Math.random() < odds;
@@ -102,6 +114,10 @@ export class HeroRoom extends Phaser.GameObjects.Container {
 			default:
 				break;
 		}
+	}
+
+	onHeroClick() {
+		// this.setHeroState(HeroState.Idle);
 	}
 
 	/* Debug */
--- src/rooms/PrincessRoom.ts
+++ src/rooms/PrincessRoom.ts
@@ -31,9 +31,10 @@ export class PrincessRoom extends Phaser.GameObjects.Container {
 		this.add(this.princessButton);
 
 		this.princessImage = scene.add.image(0, 0, 'princess_default');
+		this.princessImage.setOrigin(0.5, 1.0);
 		this.princessButton.add(this.princessImage);
 		this.princessButton.bindInteractive(this.princessImage, false);
-		this.princessButton.on("click", this.onPrincessClick, this);
+		this.princessButton.on('click', this.onPrincessClick, this);
 
 		this.timer = this.scene.time.addEvent({
 			delay: 3000,
@@ -42,11 +43,14 @@ export class PrincessRoom extends Phaser.GameObjects.Container {
 			paused: true,
 		});
 
-		this.setPrincessState(PrincessState.Idle);
+		this.setPrincessState(PrincessState.Escaping);
 	}
 
 	update(time: number, delta: number) {
-		this.princessButton.setScale(1.0 - 0.1 * this.princessButton.holdSmooth);
+		const princessHoldX = 1.0 + 0.15 * this.princessButton.holdSmooth;
+		const princessHoldY = 1.0 - 0.1 * this.princessButton.holdSmooth;
+		const princessSquish = 0.02;
+		this.princessButton.setScale((1.0 + princessSquish * Math.sin(time / 200)) * princessHoldX, (1.0 + princessSquish * Math.sin(-time / 200)) * princessHoldY);
 	}
 
 	setVisible(isShown: boolean): this {
@@ -112,17 +116,17 @@ export class PrincessRoom extends Phaser.GameObjects.Container {
 		switch (this.princessState) {
 			case PrincessState.Idle:
 				let texture1 = Phaser.Math.RND.pick(['princess_default', 'princess_plead', 'princess_stare']);
-				this.princessButton.setPosition(this.scene.CX, this.scene.CY);
+				this.princessButton.setPosition(960, 800);
 				this.princessImage.setTexture(texture1);
 				break;
 			case PrincessState.Sleeping:
 				let texture2 = Phaser.Math.RND.pick(['princess_laying', 'princess_laying_2', 'princess_laying_3']);
 				this.princessImage.setTexture(texture2);
-				this.princessButton.setPosition(0.77 * this.scene.W, 0.47 * this.scene.H);
+				this.princessButton.setPosition(1500, 730);
 				break;
 			case PrincessState.Escaping:
 				this.princessImage.setTexture('princess_escape_1');
-				this.princessButton.setPosition(0.64 * this.scene.W, 0.26 * this.scene.H);
+				this.princessButton.setPosition(1080, 500);
 				break;
 			case PrincessState.Fled:
 				this.princessButton.setVisible(false);
@@ -134,7 +138,10 @@ export class PrincessRoom extends Phaser.GameObjects.Container {
 	}
 
 	onPrincessClick() {
-		this.setPrincessState(PrincessState.Idle);
+		if (this.princessState == PrincessState.Escaping) {
+			this.setPrincessState(PrincessState.Idle);
+			this.princessImage.setTexture('princess_plead');
+		}
 	}
 
 	/* Debug */
