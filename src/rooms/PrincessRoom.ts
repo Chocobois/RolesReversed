1094439d7850ede3d4d35e3c9e78d00f7a95389f
Golen <golen87@hotmail.com>
2023-07-08 16:44:02 +0200

    Adjust sound volume

--- src/rooms/PrincessRoom.ts
+++ src/rooms/PrincessRoom.ts
@@ -152,7 +152,7 @@ export class PrincessRoom extends Room {
 	onPrincessClick() {
 		if (this.princessState == PrincessState.Escaping) {
 			this.setPrincessState(PrincessState.Idle);
-			this.scene.sound.play('CAPTURE_SOUND');
+			this.scene.sound.play('CAPTURE_SOUND', { volume: 0.1 });
 			this.princessImage.setTexture('princess_plead');
 			this.timer.paused = true;
 		}
