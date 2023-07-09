f7dbfeb33c2adbe04b8dff51df2aee6dc21eb60d
Dan-Eric Næss <dan-eric.naess@live.no>
2023-07-09 17:17:41 +0200

    Version text

--- src/scenes/TitleScene.ts
+++ src/scenes/TitleScene.ts
@@ -87,11 +87,6 @@ export class TitleScene extends BaseScene {
 		this.tap.setStroke('#FFF', 20);
 		this.tap.setPadding(2 * 10);
 
-		// this.version = this.createText(this.W, this.H, 40, "#000", version);
-		// this.version.setOrigin(1, 1);
-		// this.version.setAlpha(-1);
-		// this.version.setStroke("#FFF", 10*4);
-		// this.version.setPadding(2*40*4);
 
 		this.credits = this.add.container(this.W - 520, this.H);
 		this.credits.setVisible(false);
@@ -110,6 +105,14 @@ export class TitleScene extends BaseScene {
 		credits2.setPadding(2);
 		credits2.setLineSpacing(-18);
 		this.credits.add(credits2);
+
+		this.version = this.createText(0, this.H, 20, "#000", version);
+		this.version.setOrigin(0, 1);
+		this.version.setAlpha(0);
+		this.version.setStroke('#FFF', 6);
+		this.version.setLineSpacing(-18);
+		this.version.setPadding(2);
+		this.version.setVisible(false);
 
 		// Music
 		if (!this.musicTitle) {
@@ -153,6 +156,9 @@ export class TitleScene extends BaseScene {
 
 			if (this.credits.visible) {
 				this.credits.alpha += 0.02 * (1 - this.credits.alpha);
+			}
+			if (this.version.visible) {
+				this.version.alpha += 0.02 * (1 - this.version.alpha);
 			}
 		} else {
 			this.tap.alpha += 0.01 * (1 - this.tap.alpha);
@@ -204,6 +210,7 @@ export class TitleScene extends BaseScene {
 		if (bar >= 4) {
 			this.subtitle.setVisible(true);
 			this.credits.setVisible(true);
+			this.version.setVisible(true);
 		}
 	}
 
